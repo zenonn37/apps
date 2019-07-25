551a570a0f50151245fe9ffb6b263b9d2509cf5f
Christopher Crowd <christophercrowd@Christophers-Mac-mini.local>
2019-07-25 15:16:08 -0400

    smaill update

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Task;
 
 class TaskController extends Controller
 {
@@ -13,7 +14,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        return Task::all();
     }
 
     /**
